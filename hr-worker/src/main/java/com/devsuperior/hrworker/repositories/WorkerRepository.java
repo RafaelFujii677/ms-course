07fafb42ca1e3b50fd0f23ba5c43379b7cb6be91
Rafael Fujii <50469986+RafaelFujii677@users.noreply.github.com>
2021-07-20 11:05:42 -0300

    Delete hr-worker directory

--- hr-worker/src/main/java/com/devsuperior/hrworker/repositories/WorkerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.devsuperior.hrworker.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.devsuperior.hrworker.model.Worker;
-
-public interface WorkerRepository extends JpaRepository<Worker, Long> {
-
-}
